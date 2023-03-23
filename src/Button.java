4d810ba421c2701ed7839c37c0c6ba4c6554660f
Cieran O'Neill <cieran999@gmail.com>
2023-03-23 21:29:50 +0000

    cooler buttons

--- src/Button.java
+++ src/Button.java
@@ -1,11 +1,17 @@
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 
 public class Button extends JButton {
     
     public Button(String text){
         super(text);
+        setContentAreaFilled(false);
+        setFocusPainted(false); // used for demonstration
     }
 
     public Button bounds(int x, int y, int width, int height) {
@@ -26,5 +32,18 @@ public class Button extends JButton {
     public Button fgColour(Color fg) {
         this.setForeground(fg);
         return this;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        final Graphics2D g2 = (Graphics2D) g.create();
+        g2.setPaint(new GradientPaint(
+            new Point(0, 0), 
+            Theme.BUTTON_BG_1, 
+            new Point(0, getHeight()), 
+            Theme.BUTTON_BG_2,false));
+        g2.fillRect(0, 0, getWidth(), getHeight());
+        g2.dispose();
+        super.paintComponent(g);
     }
 }
--- src/Theme.java
+++ src/Theme.java
@@ -5,4 +5,6 @@ public abstract class Theme {
     static final Color SECONDARY_BG = Color.DARK_GRAY;
     static final Color PRIMARY_FG = Color.BLACK;
     static final Color SECONDARY_FG = Color.WHITE;
+    static final Color BUTTON_BG_1 = Color.WHITE;
+    static final Color BUTTON_BG_2 = Color.LIGHT_GRAY;
 }
