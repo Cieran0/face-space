234c865fb0e07f229d242be8b0e80ed09bef6959
Cieran O'Neill <cieran999@gmail.com>
2023-03-24 14:44:37 +0000

    example theme ig

--- src/Theme.java
+++ src/Theme.java
@@ -9,14 +9,15 @@ public abstract class Theme {
     //static final Color BUTTON_BG_1 = Color.WHITE;
     //static final Color BUTTON_BG_2 = Color.LIGHT_GRAY;
     //static final Color BUTTON_TEXT = Color.DARK_GRAY;
+    //static final Color BUTTON_SELECTED = Color.WHITE;
 
-    static final Color PRIMARY_BG = new Color(0x9E829C);
+    static final Color PRIMARY_BG = new Color(0x855e82);
     static final Color SECONDARY_BG = new Color(0x291528);
     static final Color ACCENT_BG = new Color(0x3A3E3B);
     static final Color PRIMARY_FG = Color.BLACK;
     static final Color SECONDARY_FG = new Color(0xFBACBE);
-    static final Color BUTTON_BG_1 = Color.WHITE;
-    static final Color BUTTON_BG_2 = new Color(0x9E829C);
+    static final Color BUTTON_BG_1 = new Color(0xf0bdec);
+    static final Color BUTTON_BG_2 = new Color(0x855e82);
     static final Color BUTTON_TEXT = new Color(0x291528);
     static final Color BUTTON_SELECTED = Color.WHITE;
 }
