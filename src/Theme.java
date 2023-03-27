9f8717d75b61ae2825101e5f88935ec3d283c423
M-harley01 <190006115@dundee.ac.uk>
2023-03-27 13:56:46 +0100

    new themes!

--- src/Theme.java
+++ src/Theme.java
@@ -1,23 +1,70 @@
 import java.awt.Color;
 
 public abstract class Theme {
-    //static final Color PRIMARY_BG = Color.LIGHT_GRAY;
-    //static final Color SECONDARY_BG = Color.DARK_GRAY;
-    //static final Color ACCENT_BG = Color.GRAY;
-    //static final Color PRIMARY_FG = Color.BLACK;
-    //static final Color SECONDARY_FG = Color.WHITE;
-    //static final Color BUTTON_BG_1 = Color.WHITE;
-    //static final Color BUTTON_BG_2 = Color.LIGHT_GRAY;
-    //static final Color BUTTON_TEXT = Color.DARK_GRAY;
-    //static final Color BUTTON_SELECTED = Color.WHITE;
 
-    static final Color PRIMARY_BG = new Color(0x855e82);
-    static final Color SECONDARY_BG = new Color(0x291528);
-    static final Color ACCENT_BG = new Color(0x3A3E3B);
-    static final Color PRIMARY_FG = Color.BLACK;
-    static final Color SECONDARY_FG = new Color(0xFBACBE);
-    static final Color BUTTON_BG_1 = new Color(0xf0bdec);
-    static final Color BUTTON_BG_2 = new Color(0x855e82);
-    static final Color BUTTON_TEXT = new Color(0x291528);
-    static final Color BUTTON_SELECTED = Color.WHITE;
+    //light theme
+
+//    static final Color PRIMARY_BG = Color.LIGHT_GRAY;
+//    static final Color SECONDARY_BG = Color.DARK_GRAY;
+//    static final Color ACCENT_BG = Color.GRAY;
+//    static final Color PRIMARY_FG = Color.BLACK;
+//    static final Color SECONDARY_FG = Color.WHITE;
+//    static final Color BUTTON_BG_1 = Color.WHITE;
+//    static final Color BUTTON_BG_2 = Color.LIGHT_GRAY;
+//    static final Color BUTTON_TEXT = Color.DARK_GRAY;
+//    static final Color BUTTON_SELECTED = Color.WHITE;
+
+    //blue theme
+
+//    static final Color PRIMARY_BG = new Color(0x84B2D9);
+//
+//    static final Color SECONDARY_BG = new Color(0x2C4454);
+//    static final Color ACCENT_BG = new Color(0x517D98);
+//    static final Color PRIMARY_FG = Color.BLACK;
+//    static final Color SECONDARY_FG = new Color(0xFFFFFF);
+//    static final Color BUTTON_BG_1 = new Color(0xC2C2C2);
+//    static final Color BUTTON_BG_2 = new Color(0xC2C2C2);
+//    static final Color BUTTON_TEXT = new Color(0x000000);
+//    static final Color BUTTON_SELECTED = Color.WHITE;
+
+    //red theme
+
+//    static final Color PRIMARY_BG = new Color(0xD98484);
+//
+//
+//    static final Color SECONDARY_BG = new Color(0x542C2C);
+//    static final Color ACCENT_BG = new Color(0x985151);
+//    static final Color PRIMARY_FG = new Color(0xFFFFFF);
+//    static final Color SECONDARY_FG = new Color(0xFFFFFF);
+//    static final Color BUTTON_BG_1 = new Color(0xF1B434);
+//    static final Color BUTTON_BG_2 = new Color(0xF68A2A);
+//    static final Color BUTTON_TEXT = new Color(0x000000);
+//    static final Color BUTTON_SELECTED = Color.WHITE;
+
+    //dark theme
+
+//    static final Color PRIMARY_BG = new Color(0x494949);
+//
+//
+//    static final Color SECONDARY_BG = new Color(0x2D2D2D);
+//    static final Color ACCENT_BG = new Color(0x252525);
+//    static final Color PRIMARY_FG = new Color(0xFFFFFF);
+//    static final Color SECONDARY_FG = new Color(0xFFFFFF);
+//    static final Color BUTTON_BG_1 = new Color(0xF1B434);
+//    static final Color BUTTON_BG_2 = new Color(0xF68A2A);
+//    static final Color BUTTON_TEXT = new Color(0xFFFFFF);
+//    static final Color BUTTON_SELECTED = new Color(0x000000);
+
+    //Irn bru theme
+
+    static final Color PRIMARY_BG = new Color(0x0067F1);
+
+    static final Color SECONDARY_BG = new Color(0xFF8100);
+    static final Color ACCENT_BG = new Color(0x858484);
+    static final Color PRIMARY_FG = new Color(0xFFFFFF);
+    static final Color SECONDARY_FG = new Color(0xFFFFFF);
+    static final Color BUTTON_BG_1 = new Color(0x858484);
+    static final Color BUTTON_BG_2 = new Color(0xFFFFFF);
+    static final Color BUTTON_TEXT = new Color(0x000000);
+    static final Color BUTTON_SELECTED = new Color(0xFFFFFF);
 }
